c0974e903dd214cc89abea376fc0250b4ca8ed45
mkiller4040 <66451768+mkiller4040@users.noreply.github.com>
2021-03-01 16:59:47 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import WriteStoryScreen from './screens/WriteStoryScreen'
+import ReadStoryScreen from './screens/ReadStoryScreen'
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Transaction: {screen: TransactionScreen},
+    Search: {screen: SearchScreen},
+  });
+
+const AppContainer =  createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
